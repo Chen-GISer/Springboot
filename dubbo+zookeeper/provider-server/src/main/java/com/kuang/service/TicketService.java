18f9dabc55c4c68c1cd96c507c3afc4e6ff4530f
Fosde <60883788+Chen-GISer@users.noreply.github.com>
2020-08-25 07:35:15 +0800

    Add files via upload

---
+++ dubbo+zookeeper/provider-server/src/main/java/com/kuang/service/TicketService.java
@@ -0,0 +1,11 @@
+package com.kuang.service;
+
+/**
+ * 创作者: 陈文震
+ * 创作日期: 2020年07月22日  周三  16:20
+ */
+public interface TicketService {
+
+    public String getTicket();
+
+}
---
+++ dubbo+zookeeper/provider-server/src/main/java/com/kuang/service/TicketServiceImpl.java
@@ -0,0 +1,12 @@
+package com.kuang.service;
+
+/**
+ * 创作者: 陈文震
+ * 创作日期: 2020年07月22日  周三  16:22
+ */
+public class TicketServiceImpl implements TicketService {
+    @Override
+    public String getTicket() {
+        return "《陈文震牛逼》";
+    }
+}
